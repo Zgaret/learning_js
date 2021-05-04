a9205f0bf6f6c0ed8bdd73e6d27b1f2a9dac7686
Alex <zgaret@bk.ru>
2021-05-04 15:14:30 +0300

    fix errors

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const persinalMovieDB = {
 };
 
 let lastMovie = prompt('Один из последних просмотренных фильмов?');
-persinalMovieDB[movies] = lastMovie;
+persinalMovieDB.movies[lastMovie];
 let filmScore = +prompt('На сколько его оцените?');
 persinalMovieDB.movies[lastMovie] = filmScore;
 console.log(persinalMovieDB.movies);
