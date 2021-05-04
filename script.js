6a7270fae2eaad0e613265399bf8ed0be9690b16
Alex <zgaret@bk.ru>
2021-05-04 14:58:08 +0300

    add object wiht a description about movies

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 'use strict';
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+
+const persinalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
